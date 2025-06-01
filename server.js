8f68851519c193b2996e58d51804a20a0a0bb18c
Tales Santos <detysantosos61@gmail.com>
2025-06-01 20:03:19 -0300

    Upgrade files

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 require("dotenv").config();
+const path = require("path");
 const app = express();
 const tokensSalvos = {};
 const { MercadoPagoConfig, Preference, Payment } = require("mercadopago");
